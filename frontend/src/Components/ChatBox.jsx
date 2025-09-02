6c8446979d5d68815894064c40ae3db2d395c76d
parastooMajidi.parastoo <parastoomajidi.parasto.majidi0@gmail.com>
2025-09-02 19:27:19 +0330

    add useState and useEffects

---
+++ frontend/src/Components/ChatBox.jsx
@@ -0,0 +1,40 @@
+
+import React, {useState, useEffect} from "react";
+import {io} from 'socket.io-client';
+import '../Styels/ChatBox.css';
+
+const socket = io("http://localhost:4000");
+
+export function ChatBBox({userId, isAdmin = false, currentChatUserId = null}){
+    const [messages, setMessages] = useState([]);
+    const [input, setInput]=useEffect([]);
+    const [activUser, setActiveUser]=useState([]);
+
+    const targetUserId = isAdmin ? currentChatUserId:userId
+
+
+
+}
+const sendMessage=()=>{
+    if(input.trim() && targetUserId){
+        const newMsg={
+            text:input,
+            sender: isAdmin? "admin": "user",
+            timestamp: new Date().toISOString(),
+            id:Date.now(),
+        };
+        setMessages(prev =>[...prev, newMesg]);
+        socket.emit('send_message',{
+            ...newMsg,
+            userId:targetUserId
+        });
+        setInput("");
+    }
+}
+
+useEffect(()=>{
+    if(!targetUserId)return;
+    setMessages([]);
+
+},[])
+
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ChatBox from "../Components/ChatBox"
+
+export const Home=()=>{
+    return (
+        <div className="home_container">
+            <ChatBox userId="USER_UNIQUE_ID"/>
+
+        </div>
+    )
+}
